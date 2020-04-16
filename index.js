2eaadba83a326acf4e5401eb866fa31fa5118025
Karen Lei <karenwinnielei@gmail.com>
2020-04-16 16:54:09 -0700

    complete appleIndex challenge

--- index.js
+++ index.js
@@ -141,9 +141,6 @@ function getName(person) {
  * passing in [ 'orange', 'grape', 'apple', 'banana', 'mango' ] as the argument,
  * the returned value should be: 2.
 */
-function appleIndex(/* code here */) {
-  /* code here */
-}
 
 function appleIndex(array){
   return array.indexOf('apple');
